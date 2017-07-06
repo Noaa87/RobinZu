110610b17e272d0cadc5746540cff02d0a0785b6
Wnns <wnnspro@gmail.com>
2017-07-06 11:57:31 +0200

    first commit

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php
+
+	if(isset($_POST['name'])){
+
+		$to      = '';
+		$subject = 'Contact';
+		$message = 'Name: ' . $_POST['name'] . PHP_EOL . 
+		'Email: ' . $_POST['email'] . PHP_EOL . 
+		'Phone: ' . $_POST['phone'] . PHP_EOL . 
+		'Contact reason: ' . $_POST['topic'] . PHP_EOL . 
+		'Message: ' . $_POST['message'];
+
+
+		mail($to, $subject, $message);
+
+		echo "Thank you for contacting";
+	}
